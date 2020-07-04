d47a39b4ddc3956f60cf51eb8e4c744eceb35eb0
LEvinson2504 <ldsouza2504@gmail.com>
2020-07-04 13:56:10 +0530

    created TODO

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import styles from "./App.module.css";
 import { getUpcomingMatches, getMatchDetails } from "./api";
 
 import db from "./firebase";
+import { queryByRole } from "@testing-library/react";
 const firebase = require("firebase");
 require("firebase/firestore");
 export default class App extends Component {
@@ -19,16 +20,53 @@ export default class App extends Component {
   async componentDidMount() {
     const upcomingMatches = await getUpcomingMatches();
     const users = await this.getLeaderboard();
-    // for the matches stored check: 
+    // TODO automated poinst system
+    this.checkWinners();
+    // for the matches stored check:
     // console.log("match:", match);
-    this.setState({ matches: upcomingMatches, isFetching: false, users: users });
+    this.setState({
+      matches: upcomingMatches,
+      isFetching: false,
+      users: users,
+    });
     // console.log(this.state.match);
   }
 
-  checkWinners() {
+  async checkWinners() {
+    // function to delete the match from database:
+    // use query to match the id of match to delete
+
+    // (function deleteMatchFromDB(id) {
+    //   db.collection("matches")
+    //     .where("upcoming.someMatches[0].id", "==", id)
+    //     .get()
+    //     .then((querySnapshot) => {
+    //       querySnapshot.forEach((doc) => {
+    //         console.log("match to delete: ", doc.data());
+    //       });
+    //     });
+    //   console.log("match is here");
+    // })(264659);
+
     // check matches in database
-    getMatchDetails(64)
-      .then(match => {
+    function getStoredMatchId() {
+      const matches = [];
+      db.collection("matches")
+        .get()
+        .then(function (querySnapshot) {
+          querySnapshot.forEach(function (doc) {
+            // console.log("db matches: ", doc.data())
+            let storedMatches = doc.data().someMatches;
+            storedMatches.forEach((match) => matches.push(match.id));
+          });
+        });
+      return matches;
+    }
+
+    // getMatchDetails(64)
+    const storedMatchIds = await getStoredMatchId();
+    storedMatchIds.forEach((match) =>
+      getMatchDetails(match).then((match) => {
         // if a match has status finished, get the result
         if (match.status === "FINISHED") {
           console.log(match.score.fullTime.homeTeam);
@@ -36,28 +74,32 @@ export default class App extends Component {
           const trueHomeScore = match.score.fullTime.homeTeam;
           const trueAwayScore = match.score.fullTime.awayTeam;
           // compare the fetched result with every user
-          db.collection("users").get().then(function (querySnapshot) {
-            querySnapshot.forEach(function (doc) {
-              const userHome = doc.data().bets[0].home.goals;
-              const userAway = doc.data().bets[0].away.goals;
-              if ((trueHomeScore === userHome) && (trueAwayScore === userAway)) {
-                // update score
-                db.collection("users")
-                  .doc(doc.data().username)
-                  .update({
-                    points: firebase.firestore.FieldValue.increment(1)
-                  })
-              }
-              // console.log(`doc data : ${doc.data().username} - ${doc.data().bets[0].home}`);
+          db.collection("users")
+            .get()
+            .then(function (querySnapshot) {
+              querySnapshot.forEach(function (doc) {
+                const userHome = doc.data().bets[0].home.goals;
+                const userAway = doc.data().bets[0].away.goals;
+                if (trueHomeScore === userHome && trueAwayScore === userAway) {
+                  // update score
+                  db.collection("users")
+                    .doc(doc.data().username)
+                    .update({
+                      points: firebase.firestore.FieldValue.increment(1),
+                    });
+                  console.log("user wins here:");
+                }
+                // console.log(`doc data : ${doc.data().username} - ${doc.data().bets[0].home}`);
+              });
+            });
 
-            })
-          })
+          // delete match from database;
         }
       })
+    );
+    // getstored match ids:
     // console.log(getMatchDetails(64));
     // if a match has status scheduled do nothing
-
-
 
     // if the result is same point + 1
 
@@ -80,14 +122,20 @@ export default class App extends Component {
     // using firestore
     db.collection("users")
       .doc(name)
-      .set({
-        username: name,
-        bets: this.state.bets,
-      }, {merge: true})
+      .set(
+        {
+          username: name,
+          bets: this.state.bets,
+        },
+        {
+          merge: true,
+        }
+      )
       .then((doc) => console.log("added", doc.id))
       .catch((err) => console.log(err));
     // this.setState({ bets: [] });
 
+    // TODO make document model easier to query data from
     // store matches
     // db.collection("matches")
     //   .doc("upcoming")
@@ -104,7 +152,17 @@ export default class App extends Component {
 
   stateHandler({ name, id, home, away }) {
     this.setState(
-      (state) => ({ name: name, bets: [...state.bets, { id, home, away }] }),
+      (state) => ({
+        name: name,
+        bets: [
+          ...state.bets,
+          {
+            id,
+            home,
+            away,
+          },
+        ],
+      }),
       () => this.storeInFirebase()
     );
   }
@@ -113,19 +171,22 @@ export default class App extends Component {
     // let usernamePromise = await db.collection("users").doc("glynel").get().data();
     // console.log("doc data: ", usernamePromise.username);
     let users = [];
-    await db.collection("users").get().then(function (querySnapshot) {
-      querySnapshot.forEach(function (doc) {
-        // console.log(`doc data : ${doc.data().username} - ${doc.data().bets[0].home}`);
-        users.push({
-          "name": doc.data().username,
-          "points": doc.data().points,
-          "homeName": doc.data().bets[0].home.name,
-          "homeGoals": doc.data().bets[0].home.goals,
-          "awayName": doc.data().bets[0].away.name,
-          "awayGoals": doc.data().bets[0].away.goals,
+    await db
+      .collection("users")
+      .get()
+      .then(function (querySnapshot) {
+        querySnapshot.forEach(function (doc) {
+          // console.log(`doc data : ${doc.data().username} - ${doc.data().bets[0].home}`);
+          users.push({
+            name: doc.data().username,
+            points: doc.data().points,
+            homeName: doc.data().bets[0].home.name,
+            homeGoals: doc.data().bets[0].home.goals,
+            awayName: doc.data().bets[0].away.name,
+            awayGoals: doc.data().bets[0].away.goals,
+          });
         });
       })
-    })
       .catch(function (error) {
         console.log("Error getting document:", error);
       });
@@ -157,23 +218,23 @@ export default class App extends Component {
     const { matches } = this.state;
     return (
       <div>
-        <LeaderBoard users={this.state.users} />
-        {/* <h1> <span role="img">👑 </span>Leader: {this.state.leader} - 2pts</h1> */}
+        <LeaderBoard users={this.state.users} />{" "}
+        {/* <h1> <span role="img">👑 </span>Leader: {this.state.leader} - 2pts</h1> */}{" "}
         <div className={styles.container}>
-          {/* <Nav /> */}
-          {/* need to handle bets  */}
+          {" "}
+          {/* <Nav /> */} {/* need to handle bets  */}{" "}
           <Matches
             matches={matches}
             handleBets={(obj) => this.stateHandler(obj)}
             handleSubmit={() => this.handleSubmit()}
-          />
+          />{" "}
           {/* <Match
-          match={match}
-          handleBets={(obj) => this.stateHandler(obj)}
-          handleSubmit={() => this.handleSubmit()}
-        /> */}
-          {/* <Matches /> <br /> */}
-        </div>
+                    match={match}
+                    handleBets={(obj) => this.stateHandler(obj)}
+                    handleSubmit={() => this.handleSubmit()}
+                  /> */}{" "}
+          {/* <Matches /> <br /> */}{" "}
+        </div>{" "}
       </div>
     );
   }
